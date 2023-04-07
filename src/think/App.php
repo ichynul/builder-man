44eb335e085b9567cb55496ad255c3e1c9d16263
ichynul <ichynul@163.com>
2023-04-07 19:03:01 +0800

    fix getDefaultLang

--- src/think/App.php
+++ src/think/App.php
@@ -71,6 +71,6 @@ class App
      */
     public static function getDefaultLang()
     {
-        return config('lang.default_lang', 'zh-cn');
+        return config('plugin.builder.man.app.lang.default_lang', 'zh-cn');
     }
 }
