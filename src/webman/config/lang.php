97128aaa027c8163f5bb2db0097f8ef416dcd984
ichynul <ichynul@163.com>
2025-10-18 12:04:37 +0800

    调整默认语言为zh-cn

--- src/webman/config/lang.php
+++ src/webman/config/lang.php
@@ -3,7 +3,7 @@
 //langauge config for \think\Lang
 return [
     // 默认语言
-    'default_lang'    => 'en',
+    'default_lang'    => 'zh-cn',
     // 扩展语言包
     'extend_list'     => [],
     // 是否支持语言分组
